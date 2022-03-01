934bce74bf8ff7ef8c005e539d0805e6db101010
Eli Chien <46387845+elichienxD@users.noreply.github.com>
2022-03-01 08:03:25 +0800

    Update hyperparameters

--- src/run_AllSetTransformer.sh
+++ src/run_AllSetTransformer.sh
@@ -122,7 +122,7 @@ do
             --All_num_layers 1 \
             --MLP_num_layers 2 \
             --feature_noise 0.0 \
-            --heads 8 \
+            --heads 1 \
             --Classifier_num_layers 1 \
             --MLP_hidden 64 \
             --Classifier_hidden 64 \
